e8cc61232b2c98ee65121c9f5fbdebd26fb317a9
Jawad Ahmad <ja1594355@gmail.com>
2025-09-19 00:17:48 +0500

    compeleted chatapp with continous loopback messsaging

--- Networking/chatapp/client.js
+++ Networking/chatapp/client.js
@@ -8,6 +8,24 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
+function clearLine(direction) {
+  return new Promise((resolve, rejects) => {
+    process.stdout.clearLine(direction, () => {
+      resolve();
+    });
+  });
+}
+
+function moveCursor(dx, dy) {
+  return new Promise((resolve, rejects) => {
+    process.stdout.moveCursor(dx, dy, () => {
+      resolve();
+    });
+  });
+}
+
+// ==============================================================
+
 // client is a stream && client is socket is (when two computer connect with each other)
 const socket = net.createConnection(
   {
@@ -17,18 +35,42 @@ const socket = net.createConnection(
 
   //   when connected to the server running at {host , port}
   async () => {
+    let id;
     console.log("connected to the server");
+    async function ask() {
+      // after connection we ask the user to enter a message
+      const message = await rl.question("Enter Message>");
+      // after input recived clear the line
+      await moveCursor(0, -1);
+      await clearLine(0);
 
-    const message = await rl.question("Enter Message>");
-    socket.write(message);
+      // send to the server
+      socket.write(`${id}-message-${message}`);
+    }
+
+    //  where where the server sends some data we are going to trigger this
+    // event , this events reads data from the server and after reading it
+    // again write (sends ) data to the server
+    // if server recive some data it well again sends up back
+    // its like recursive
+    socket.on("data", async (data) => {
+      console.log(); // just a extra line
+      await moveCursor(0, -1);
+      await clearLine(0);
+
+      if (data.toString("utf-8").substring(0, 2) === "id") {
+        // form 3 to end
+        id = data.toString("utf-8").substring(3);
+        console.log(`My id : ${id}`);
+      } else {
+        // message from the server to print
+        console.log(data.toString("utf-8"));
+      }
+
+      ask();
+    });
   }
 );
-
-socket.on("data", (chunk) => {
-  console.log("From Server =>", chunk.toString("utf-8"));
-  console.log();
-});
-
 // events when something happens (EventEmiter)
 // just defining events that are going to emit(by the socket)
 socket.on("close", () => {
--- Networking/chatapp/server.js
+++ Networking/chatapp/server.js
@@ -1,22 +1,48 @@
 //  chat server
-
 import net from "net";
+
+// ==========================================================
+
+/**
+ *
+ * Recursive message principle
+ * Read Data
+ * within the reading send it the client
+ *
+ */
 
 // server is noting just a stream
 const server = net.createServer();
 const clients = [];
+
 // server also inherit from the evet emitter
 
 // when ever the connection is made run the callback (callbacks are stored in eventloop)
 server.on("connection", (socket) => {
-  console.log(`new Connection`);
+  let client_id = clients.length + 1;
+  // just give an id to whom ever connect
+  // so this is going to be a personal message
+  socket.write(`id-${client_id}`);
+
+  console.log(`new Connection of id :${client_id}`);
+
+  //   reads data from the connected client(socket) and send some data (buffer) to it
+
+  //   you can see that its also a bit recursive as soon as we read data from the client we again send it back
   socket.on("data", (data) => {
-    clients.map((s) => {
-      s.write(data);
+    // just stucturing the recived message (id-message-(actual message))
+    const dataString = data.toString("utf-8");
+    const id = dataString.substring(0, dataString.indexOf("-"));
+    const message = dataString.substring(dataString.indexOf("-message-") + 9);
+
+    // maping through each client we broadcast the message
+    clients.map((sokObj) => {
+      sokObj.socket.write(`> User ${id} : ${message}`);
     });
   });
 
-  clients.push(socket);
+  //   storing all the info of the clients
+  clients.push({ id: client_id.toString(), socket });
 });
 
 // (important) at this port and ipaddress our server is running
